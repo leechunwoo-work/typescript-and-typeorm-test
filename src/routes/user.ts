067b5a92e3cfdd7802d55e1b32c7ca83f3bc4235
chunwoolee.work <chunwoolee.work@gmail.com>
2022-11-24 16:16:43 +0900

    feat: 회원탈퇴 추가

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { user } from '../controllers';
+import tokenVerify from '../middlewares/tokenVerify';
 
 const router = Router();
 
 router.post('/signUp', user.signUp);
 router.post('/signIn', user.signIn);
+router.delete('/signOut', tokenVerify, user.signOut);
 router.get('/duplicateCheckBy', user.duplicateCheckBy);
 
 export default router;
